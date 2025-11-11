2e33bb00826bcb07897aeab26cf52ae895729151
Arno Bouiron <arno.bouiron@gmail.com>
2025-11-11 13:46:16 +0100

    UserRepository et FileRepository added

--- app/src/main/java/com/arnoagape/polyscribe/data/repository/FileRepository.kt
+++ app/src/main/java/com/arnoagape/polyscribe/data/repository/FileRepository.kt
@@ -1,4 +1,37 @@
 package com.arnoagape.polyscribe.data.repository
 
-class FileRepository {
+import com.arnoagape.polyscribe.data.service.file.FileApi
+import com.arnoagape.polyscribe.domain.model.File
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Repository responsible for managing file-related operations.
+ * It delegates data access to the [FileApi] (FirebaseFileApi implementation),
+ * providing a clean abstraction layer for ViewModels.
+ */
+@Singleton
+class FileRepository @Inject constructor(
+    private val fileApi: FileApi
+) {
+
+    /**
+     * Observes the list of all files ordered by descending creation date.
+     */
+    fun getFilesOrderByCreationDateDesc(): Flow<List<File>> =
+        fileApi.getFilesOrderByCreationDateDesc()
+
+    /**
+     * Uploads a new file to Firebase (Storage + Firestore).
+     * @throws java.io.IOException if there is no internet connection
+     * @throws IllegalArgumentException if the file type or size is invalid
+     */
+    suspend fun sendFile(file: File) = fileApi.sendFile(file)
+
+    /**
+     * Observes a single file by its unique ID.
+     */
+    fun getFileById(fileId: String): Flow<File?> =
+        fileApi.getFileById(fileId)
 }
--- app/src/main/java/com/arnoagape/polyscribe/data/repository/UserRepository.kt
+++ app/src/main/java/com/arnoagape/polyscribe/data/repository/UserRepository.kt
@@ -1,4 +1,24 @@
 package com.arnoagape.polyscribe.data.repository
 
-class UserRepository {
+import com.arnoagape.polyscribe.data.service.user.UserApi
+import com.arnoagape.polyscribe.domain.model.User
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * This class provides a repository for accessing and managing User data.
+ * It utilizes dependency injection to retrieve a UserApi instance for interacting
+ * with the data source. The class is marked as a Singleton using @Singleton annotation,
+ * ensuring there's only one instance throughout the application.
+ */
+@Singleton
+class UserRepository @Inject constructor(private val userApi: UserApi) {
+
+    fun observeUser(): Flow<User?> = userApi.observeUser()
+    suspend fun updateUser(user: User) = userApi.updateUser(user)
+    suspend fun ensureUserInFirestore() = userApi.ensureUserInFirestore()
+    fun signOut() = userApi.signOut()
+    fun isUserSignedIn(): Flow<Boolean> = userApi.isUserSignedIn()
+    suspend fun deleteUser() = userApi.deleteUser()
 }
