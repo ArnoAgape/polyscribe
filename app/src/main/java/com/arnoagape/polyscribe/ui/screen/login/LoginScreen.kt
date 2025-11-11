239953ec3c09f0eddfa58919f93aaddc76a3ad41
Arno Bouiron <arno.bouiron@gmail.com>
2025-11-11 18:45:49 +0100

    Logo added on loading page of LoginScreen

--- app/src/main/java/com/arnoagape/polyscribe/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/arnoagape/polyscribe/ui/screen/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.arnoagape.polyscribe.ui.screen.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -33,10 +34,9 @@ fun LoginPlaceholderScreen() {
             contentAlignment = Alignment.Center
         ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Icon(
+                Image(
                     painter = painterResource(R.drawable.ic_logo),
                     contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
                     modifier = Modifier.size(64.dp)
                 )
                 Spacer(Modifier.height(16.dp))
