5b26bd1a52ff8fc36dd4f0f3eb7bb4b08453326e
Arno Bouiron <arno.bouiron@gmail.com>
2025-11-11 17:27:26 +0100

    custom ic_launcher added

--- app/src/main/java/com/arnoagape/polyscribe/MainActivity.kt
+++ app/src/main/java/com/arnoagape/polyscribe/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -93,7 +92,6 @@ enum class AppDestinations(
     val icon: ImageVector,
 ) {
     HOME("Home", Icons.Default.Home),
-    FAVORITES("Favorites", Icons.Default.Favorite),
     PROFILE("Profile", Icons.Default.AccountBox),
 }
 
