778432839f63c8e56c6923dd26dd96e8dcdc3149
Arno <arno.bouiron@gmail.com>
2026-03-12 16:02:13 +0100

    dependencies updated

--- app/src/main/java/com/arnoagape/polyscribe/ui/common/components/PickerField.kt
+++ app/src/main/java/com/arnoagape/polyscribe/ui/common/components/PickerField.kt
@@ -1,13 +1,9 @@
 package com.arnoagape.polyscribe.ui.common.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
